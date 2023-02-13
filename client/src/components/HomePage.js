37b38007325bc30d3c37101f99ea5d8a81ef4b4c
Jason Njoku <JasNjoku@gmail.com>
2023-02-13 19:57:57 +0000

    Added media query breakpoints

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div class="homepage-container">
+        <div className="content-top">
+          <nav className="top-nav">
+            <div className="nav-content-left">
+              <Link id="logo" to={'/'}>Logo</Link>
+            </div>
+            <div className="nav-content-right">
+              <Link id="linkToSignIn" to={'/'}>Sign In</Link>
+              <Link id="linkToAccount" to={'/'}>Account</Link>
+            </div>
+          </nav>
+          <header className="header-container">
+            <nav className="bottom-nav">
+              <div>
+                
+              </div>
+            </nav>
+          </header>
+        </div>
       </div>
     )
   }
