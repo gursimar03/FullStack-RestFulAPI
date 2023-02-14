dc25cca5152fc7cf178175f9bb3d05d2a0d2e1f2
Jason Njoku <JasNjoku@gmail.com>
2023-02-14 00:11:14 +0000

    Starting the mobile website design

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,28 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div class="homepage-container">
-        <div className="content-top">
-          <nav className="top-nav">
-            <div className="nav-content-left">
-              <Link id="logo" to={'/'}>Logo</Link>
+      <div className="homepage-container">
+
+        <div className="homepage-body">
+          <div className="homepage-hero">
+            {/* <img className="hero-image"  alt="homepage" src={require('../images/Hero-Image.jpg')}/> */}
+            <div className="homepage-hero-info">
+
             </div>
-            <div className="nav-content-right">
-              <Link id="linkToSignIn" to={'/'}>Sign In</Link>
-              <Link id="linkToAccount" to={'/'}>Account</Link>
-            </div>
-          </nav>
-          <header className="header-container">
-            <nav className="bottom-nav">
-              <div>
-                
-              </div>
-            </nav>
-          </header>
+          </div>
+          <main>
+
+          </main>
         </div>
+
       </div>
     )
   }
