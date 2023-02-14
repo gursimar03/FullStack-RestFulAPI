497742a1dfe4d5832853ad3b45485396505ce8b5
Jason Njoku <JasNjoku@gmail.com>
2023-02-14 00:10:39 +0000

    Updating Routes and adding Constant Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,52 @@
-import React, {Component} from "react"
-import {BrowserRouter, Switch, Route} from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaRegUserCircle } from 'react-icons/fa';
 
-import HomePage from "./components/HomePage"
+import HomePage from "./components/HomePage";
 
 
-import {ACCESS_LEVEL_GUEST} from "./config/global_constants"
+import { ACCESS_LEVEL_GUEST } from "./config/global_constants";
 
 
-if (typeof localStorage.accessLevel === "undefined")
-{
+if (typeof localStorage.accessLevel === "undefined") {
     localStorage.name = "GUEST"
     localStorage.accessLevel = ACCESS_LEVEL_GUEST
     localStorage.token = null
 }
 
-    
-export default class App extends Component 
-{
-    render() 
-    {
+class App extends React.Component {
+    render() {
         return (
-            <BrowserRouter>
-                <Switch>           
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="*" component={HomePage}/>                            
-                </Switch>
-            </BrowserRouter>
+            <div className="App">
+                <BrowserRouter>
+                    <div className="content-top">
+                        <nav className="top-nav">
+                            <div className="nav-content-left">
+                                <Link id="logo" to={'/'}>Logo</Link>
+                            </div>
+                            <div className="nav-content-right">
+                                <Link id="linkToSignIn" to={'/'}><p>Sign In</p></Link>
+                                <Link id="linkToAccount" to={'/'}><FaRegUserCircle className="account-icon" /></Link>
+                            </div>
+                        </nav>
+                        <header className="header-container">
+                            <nav className="bottom-nav">
+                                
+                            </nav>
+                            <div>
+
+                            </div>
+                        </header>
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<HomePage />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+
         )
     }
 }
+
+export default App;
