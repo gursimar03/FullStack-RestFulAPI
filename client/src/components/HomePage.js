29822c216c5ccb074f559bb3202d6c8b1357ab91
gursimar03 <gursimar03@gmail.com>
2023-02-14 13:44:23 +0000

    home page logIn Registration and logout done

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react"
 import {Link} from "react-router-dom"
-
+import Register from "./Register"
 import axios from "axios"
-
+import {SERVER_HOST} from "../config/global_constants"
+import Login from "./Login"
+import Logout from "./Logout"
 
 export default class HomePage extends Component 
 {
@@ -20,6 +22,12 @@ export default class HomePage extends Component
   
     render() 
     {   
-        return (0)
+        return (
+            <div>
+              {localStorage.accessLevel > 0 ? <Logout />  : <div> <Register/> <Login /> </div>}
+              
+            </div>
+
+        )
     }
 }
