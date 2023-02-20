183e3c3642eac36aa659bad14696ac443078ab05
Jason Njoku <JasNjoku@gmail.com>
2023-02-20 14:39:54 +0000

    created gender routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -42,6 +42,52 @@ router.get(`/products`, (req, res) => {
     })
 })
 
+router.get(`/shoes/men`, (req, res) => {
+    productsModel.find({age: 'Men'}, (error, data) => {
+        if (data) {
+            res.json(data)
+        } else {
+            res.json(error)
+        }
+    })
+})
+
+router.get(`/shoes/women`, (req, res) => {
+    productsModel.find({age: 'Women'}, (error, data) => {
+        if (data) {
+            res.json(data)
+        } else {
+            res.json(error)
+        }
+    })
+})
+
+//get both boys and girls
+router.get(`/shoes/kids`, (req, res) => {
+    productsModel.find({}, (error, data) => {
+        if (data) {
+            //filter only boys and girls shoes
+            let kids = [];
+            data.forEach((product) => {
+                if(product.age === 'Boys' || product.age === 'Girls') {
+                    kids.push(product);
+                }
+            })
+            res.json(kids)
+        } else {
+            res.json(error)
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
 router.get(`/products/:_id`, (req, res) => {
     productsModel.findById(req.params._id, (error, data) => {
         if (data) {
@@ -51,6 +97,5 @@ router.get(`/products/:_id`, (req, res) => {
         }
     })
 })
-
 
 module.exports = router
