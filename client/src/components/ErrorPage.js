8d522ab3f8580e009bf03e83c545d3e4a64a0bc7
Jason Njoku <JasNjoku@gmail.com>
2023-03-11 16:10:39 +0000

    created error page

---
+++ client/src/components/ErrorPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ErrorPage extends React.Component {
+    render() {
+        return (
+            <div className="error-page">
+                <h1>
+                    Sorry this page does not exist
+                </h1>
+            </div>
+        )
+    }
+}
+
+
+export default ErrorPage;
