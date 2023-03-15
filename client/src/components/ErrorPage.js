157fb6b8e1a73ecb900143ecae2bc8b5e6b00298
Jason Njoku <JasNjoku@gmail.com>
2023-03-15 16:01:27 +0000

    made error page

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ScrollToTop from "../ScrollToTop";
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 class ErrorPage extends React.Component {
     render() {
         return (
             <div className="error-page">
                 <ScrollToTop />
-                <h1>
-                    Sorry this page does not exist
-                </h1>
+                <FaHome className="home-favicon"/>
+                <Link to={'/'}>Back to home</Link>
             </div>
         )
     }
