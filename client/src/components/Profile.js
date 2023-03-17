c6bdf5d082df2bceeaa5a9594667d5020cc01554
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-17 16:43:50 +0000

    Users Model Tested

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -50,7 +50,7 @@ export default class Profile extends Component {
         // // eslint-disable-next-line
         // const email = decodedToken.email // get the user's email from the decoded token
         // get the user's profile from the database using the user's email address and set the state with the user's profile data
-        axios.get(`${SERVER_HOST}/users/profile/${email}`)
+        axios.get(`${SERVER_HOST}/users/profile/${localStorage.getItem('email')}`)
             .then(res => {
                 if (res.data) {
                     if (res.data.errorMessage) {
